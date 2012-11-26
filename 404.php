3d0391bbc1a139038c605a325fbcebed04569247
Will Waywell <willwaywell@gmail.com>
2012-11-26 23:19:00 +0000

    Small text update to 404 page

--- 404.php
+++ 404.php
@@ -4,9 +4,10 @@
 	<section id="articles">
 			<article id="post-0" class="post no-results not-found">
 				<header class="post-header">
-					<h2 class="post-title">Four, oh: four.</h2>
+					<h2 class="post-title">Page not found</h2>
 				</header>
 				<div class="post-content">
+					<h2>Four, oh: four.</h2>
 					<p>Sorry, but we could not find the page you are requesting.</p>
 				</div>
 			</article>

73b67b72a18d30cba6341a9d041038f197e8665c
Will Waywell <willwaywell@gmail.com>
2012-11-27 17:29:34 +0000

    Cleaned up and styled blog posts navigation

--- functions.php
+++ functions.php
@@ -279,8 +279,9 @@ function nmd_post_links() {
 
 	if ( $wp_query->max_num_pages > 1 ) : ?>
 		<nav id="post-nav">
-			<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'twentyeleven' ) ); ?></div>
-			<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'twentyeleven' ) ); ?></div>
+			<span class="nav-previous"><?php next_posts_link( '<span class="meta-nav">&larr;</span> Older posts' ); ?></span>
+			<span class="nav-next"><?php previous_posts_link( 'Newer posts <span class="meta-nav">&rarr;</span>' ); ?></span>
+			<div class="clear"></div>
 		</nav><!-- #nav-above -->
 	<?php endif;
 	
